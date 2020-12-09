8230d2de07f4e07cc07fdfb3bef9183a2744111a
Ivana <56759682+ivanavelimirovic13@users.noreply.github.com>
2020-12-09 16:11:01 +0100

    promenaprijave

--- prijava.js
+++ prijava.js
@@ -1,17 +1,38 @@
-$(function() {
+const form = document.getElementById('forma');
+const ime = document.getElementById('Ime');
+const prezime = document.getElementById('Prezime');
+const password = document.getElementById('password');
+const greska = document.getElementById('greska');
 
+form.addEventListener('submit', (e) => {
 
-    function funk() {
-        alert("jjyhgrfe");
-    };
+    let messges = [];
 
-    $("#potvrdi").on("click", function() {
-        var string = $("#ime").val();
-        var duzina = string.length();
-        if (duzina < 6) {
-            funk();
-        }
-    });
+    if (!(ime.value.includes(''))) {
+        messges.push('Morate uneti ime\n');
+    }
+
+    if (!(prezime.value.includes(''))) {
+        messges.push('Morate uneti prezime\n');
+    }
+
+    if (password.value.length < 6) {
+        messges.push('Sifra mora imati bar 6 karaktera\n');
+    }
+
+    if (messges.length > 0) {
+        e.preventDefault();
+        greska.innerText = messges.join("");
+    } else {
+        var alert = window.alert('Uspesno ste se prijavili')
+        var openedWindow = window;
+        openedWindow.close();
+    }
 
 
 });
+
+function zatvori() {
+    var openedWindow = window;
+    openedWindow.close();
+}
